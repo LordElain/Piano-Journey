cc8aabe2c3a84d84a61aab432c5d4df3c33b929a
Kevin Elain <ladylordielaine@gmai.com>
2021-10-04 16:27:59 +0200

    Setting Up Midi Inputs
    Testing how this thing even works

---
+++ Piano Journey/Assets/Scripts/MovementTest.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementTest : MonoBehaviour
+{
+    public void PianoNotes (InputValue value)
+    {
+        var v = value.Get<Vector2>();
+    }
+}
