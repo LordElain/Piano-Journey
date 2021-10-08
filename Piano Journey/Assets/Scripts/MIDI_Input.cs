e5e05cbc77181d781fe79dc8e68e6e32a39c7916
Kevo Elain <ladylordielaine@gmail.com>
2021-10-08 13:56:21 +0200

    Started Note loader
    Created Note loader class

---
+++ Piano Journey/Assets/Scripts/MIDI_Input.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Minis;
+using UnityEngine;
+ using UnityEngine.InputSystem;
+ 
+ public class Midi_Input : MonoBehaviour, PianoJourney.IPlayerActions
+ {
+ PianoJourney controls;
+ 
+ 
+     private void Awake()
+     {
+        controls = new PianoJourney();
+        controls.Player.SetCallbacks(this);
+  
+     }
+
+    public void OnEnable()
+     {
+        Debug.Log("Enable");
+        controls.Enable();
+     }
+
+
+     public void OnDisable()
+     {
+        Debug.Log("Disable");
+        controls.Disable();
+     }
+
+    public void OnPianoNotes(InputAction.CallbackContext context)
+    {
+        controls.Player.PianoNotes.performed += _ => 
+        {
+            Debug.Log("PIANO ON");
+        };
+
+        controls.Player.PianoNotes.canceled += _ =>
+        {
+            Debug.Log("PIANO SCRIPT OFF");
+        };
+
+        //MidiDevice.current.
+        
+        
+    }
+
+}
---
+++ Piano Journey/Assets/Scripts/NotesController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NotesController : MonoBehaviour
+{
+
+    public int m_BPM;
+    public float m_Position;
+    public float m_TimePlayed;
+    private int[] m_Notes;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
