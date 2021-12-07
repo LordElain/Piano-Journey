52f1cefdfa7f0290c6493dfceb7b93604c03063e
Kevin "Elain" K <47543964+LordElain@users.noreply.github.com>
2021-12-07 13:19:09 +0100

    Editor Changes - Multiple blocks and size change
    - You can now generate multiple blocks
    - Changed the size of the generated blocks to fill the grid better

--- Piano Journey/Assets/Scripts/GridNote.cs
+++ Piano Journey/Assets/Scripts/GridNote.cs
@@ -10,6 +10,9 @@ public class GridNote : MonoBehaviour
     public string[] m_AllKeys;
     private Sprite m_NoteSprite;
     private SpriteRenderer m_sr;
+    public GameObject m_Note;
+
+    private bool m_LeftClick;
 
 
     // Start is called before the first frame update
@@ -17,31 +20,37 @@ public class GridNote : MonoBehaviour
     {
         CreateNoteArray();
         CreateGrid();
-        m_sr = GetComponent<SpriteRenderer>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        int x,y;
+        
+        x = Mathf.FloorToInt(15f);
+        y = Mathf.FloorToInt(15f);
         if (Input.GetMouseButtonDown(0))
         {
-            int x,y;
             //Right Hand Notes
             Vector3 mousePos = m_Camera.ScreenToWorldPoint(Input.mousePosition);
             mousePos.z = 0;
             Debug.Log("Maus X: " + mousePos.x + " Maus Y: " + mousePos.y);
-
-            Texture2D m_Tex = new Texture2D(500,250);
-            m_NoteSprite = Sprite.Create(m_Tex, new Rect(0f,0f,m_Tex.width,m_Tex.height),new Vector2(0,0),100f);
-            m_sr.sprite = m_NoteSprite;
-            m_sr.transform.position = mousePos;
+            m_LeftClick = true;
+            CreateNoteBlock(x,y,mousePos,m_LeftClick);
+            
         }
 
         if (Input.GetMouseButtonDown(1))
         {
             //Left Hand Notes
-
+            Vector3 mousePos = m_Camera.ScreenToWorldPoint(Input.mousePosition);
+            mousePos.z = 0;
+            Debug.Log("Maus X: " + mousePos.x + " Maus Y: " + mousePos.y);
+            m_LeftClick = false;
+            CreateNoteBlock(x,y,mousePos,m_LeftClick);
         }   
+        
     }
 
     private void CreateNoteArray()
@@ -67,6 +76,18 @@ public class GridNote : MonoBehaviour
         Vector3 Pos = GameObject.Find("Piano").transform.position;
         Debug.Log(StartPosition);
         grid = new Grid(120, 100, 1.85f, m_Camera, StartPosition);
+    }
+
+
+    private void CreateNoteBlock(int x, int y, Vector3 mousePos, bool mouseclick)
+    {
+        GameObject note = Instantiate(m_Note,mousePos, Quaternion.identity);
+        Texture2D m_Tex = new Texture2D(x,y);
+        m_sr = note.GetComponent<SpriteRenderer>();
+        m_NoteSprite = Sprite.Create(m_Tex, new Rect(0f,0f,m_Tex.width,m_Tex.height),new Vector2(0,0),100f);
+        m_sr.sprite = m_NoteSprite;
+        m_sr.transform.position = mousePos;
+        
     }
 
     public void saveFile()
