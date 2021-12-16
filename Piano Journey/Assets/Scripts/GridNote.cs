c93081c50063073893ec4c75ddba68b605590b80
Kevin "Elain" K <47543964+LordElain@users.noreply.github.com>
2021-12-16 13:35:57 +0100

    Getting a new grid system to coordinate better music note boxes
    - New Grid system with new Class "NoteBox" to get a better definition and so every NoteBox has their own ID

--- Piano Journey/Assets/Scripts/GridNote.cs
+++ Piano Journey/Assets/Scripts/GridNote.cs
@@ -9,6 +9,7 @@ using UnityEngine;
 public class GridNote : MonoBehaviour
 {
     Grid grid;
+    NoteBox m_NoteBox;
     public Camera m_Camera;
     public string[] m_NoteKeys;
     public string[] m_AllKeys;
@@ -36,14 +37,21 @@ public class GridNote : MonoBehaviour
     private List<Note> m_Notelist;
     private List <Chord> m_Chordlist;
 
+    private List <NoteBox> m_BoxIDList = new List<NoteBox>();
+    private int m_BoxID;
+    private int m_BoxIDCounter;
+
     private MidiFile m_File;
     public string m_Path;
+    private Vector3 TestX = new Vector3 (-1,0,0);
+
+    private bool m_bool;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        m_bool = false;
         m_NoteCounter = 0;
         CreateNoteArray();
         Vector3 StartPosition = GameObject.Find(m_AllKeys[1] + " Piano").transform.position;
@@ -100,7 +108,6 @@ public class GridNote : MonoBehaviour
             Debug.Log(m_GridHeight);
             if (m_Camera.transform.position.y >= GridOffset)
             {
-                Debug.Log("HERE");
                 m_GridHeight = m_GridHeight + 100;
                 Debug.Log(m_GridHeight  + "New Height");
                 Vector3 NewPosition= GameObject.Find(m_AllKeys[1] + " Piano").transform.position;
@@ -139,17 +146,14 @@ public class GridNote : MonoBehaviour
     {
         GameObject note = Instantiate(m_Note,mousePos, Quaternion.identity);
         var noteonEvent = new NoteOnEvent();
-        var Test = GameObject.FindWithTag("Key");
-        Debug.Log(Test);
         var PositionTest = Mathf.Floor(mousePos.x);
         Texture2D m_Tex = new Texture2D(x,y);
         m_sr = note.GetComponent<SpriteRenderer>();
         note.name = m_NoteCounter.ToString();
-        int KeyPosition = (int)PositionTest;
-      
-        Debug.Log(KeyPosition);
-        Debug.Log(m_AllKeys[KeyPosition]);
         
+    
+
+
         //CheckForParent(m_NoteCounterArray);
         m_NoteSprite = Sprite.Create(m_Tex, new Rect(0f,0f,m_Tex.width,m_Tex.height),new Vector2(0,0),100f);
         m_sr.sprite = m_NoteSprite;
@@ -166,6 +170,58 @@ public class GridNote : MonoBehaviour
         }
         
         m_sr.transform.position = grid.GetXY(mousePos);
+        
+
+        /* 
+        m_BoxID = Mathf.RoundToInt(m_sr.transform.position.x);
+
+        if (m_BoxIDList.Contains(m_BoxID) == false)
+        {
+             m_BoxIDList.Add(m_BoxID);
+        } */ 
+        if (m_bool == false)
+        {
+            m_NoteCounter = 0;
+            for (int i = 0; i < 120; i++)
+            {
+                if (m_NoteCounter <= 5)
+                {
+                    m_BoxIDList.Add(new NoteBox{BoxID = i, BoxPos = TestX.x});
+                    TestX.x = TestX.x+2f;
+                    m_NoteCounter++;
+                }
+                else
+                {
+                    TestX.x = TestX.x+1f;
+                    m_NoteCounter = 0;
+                }
+                
+            }
+            m_bool = true;
+        }
+        Debug.Log("Rounded Position: " + Mathf.RoundToInt(m_sr.transform.position.x));
+        NoteBox result = m_BoxIDList.Find(
+            delegate(NoteBox nb)
+            {
+                
+                return nb.BoxPos == Mathf.RoundToInt(m_sr.transform.position.x);
+            }
+
+        );
+
+        if(result != null)
+        {
+            Debug.Log(result.BoxID);
+            Debug.Log(m_AllKeys[result.BoxID]);
+        }
+        else
+        {
+            Debug.Log("ERROR");
+            Debug.Log(result);
+        }
+
+
+        
         if (!m_NoteCounterArray.Equals(note))
         {
             for (int i = 0; i < m_NoteCounterArray.Count; i++)
---
+++ Piano Journey/Assets/Scripts/NoteBox.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteBox
+{
+   public int BoxID {get; set;}
+   public float BoxPos {get;set;} 
+   
+}
